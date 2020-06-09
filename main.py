671f0b0ce4b48d1ebac9c1ad64379e65a4e207c3
pjlapinski <przemyslaw.j.lapinski@gmail.com>
2020-06-09 14:27:25 +0200

    comments for future dev

--- main.py
+++ main.py
@@ -18,6 +18,10 @@ def index():
 def sheet(sheet_name):
     # first query the database and save the file from it into the /sheets folder
     # if there are no results, return an error page
+    #
+    # or, even better, create a database handler which, when queried, returns
+    # the character sheet as a json already, without having to save it to a folder
+    # ---- this would be much preferred
     with open(os.path.join(APP_ROOT, f'sheets/{sheet_name}.json'), 'r') as f:
         result = json.loads(f.read())
     return render_template('character-sheet.html', data=result)
