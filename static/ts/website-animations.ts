ebefbce2eec6dbef75becb839c57fb439860261c
wohajo <m.prawda99@gmail.com>
2020-06-27 17:29:18 +0200

    added sidemenu sections

--- static/ts/website-animations.ts
+++ static/ts/website-animations.ts
@@ -1,15 +1,23 @@
 // this file is for CSS purpouses only 
 // the name is a subject to change
 
-let toggleable = document.getElementsByClassName("sheet-section");
+var sideMenuButtons: Array<Element> = Array.from(document.getElementsByClassName("nav-wrapper"));
+var sheetSections: Array<Element> = Array.from(document.getElementsByClassName("sheet-section"));
 initializeSideMenu()
 
-function initializeSideMenu() {
-    Array.from(toggleable).forEach(element => {
-        element.addEventListener("click", testFunction, false)
+// indexes 0-7 are for infomations about a character, 8,9 are for save and return
+
+function initializeSideMenu(): void {
+    sideMenuButtons.forEach(element => {
+        element.addEventListener("click", changeSelectedInformations, false)
     });
+    sheetSections.forEach(element => {
+        element.setAttribute("style", "display: block");
+    });
+    
+    sheetSections[0].setAttribute("style", "display: block");
 }
 
-function testFunction() {
+function changeSelectedInformations(): void {
     console.log("test")
 }
