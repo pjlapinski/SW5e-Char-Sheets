c16d70ff45eed06bafcbb253a10e2138fc7067c2
wohajo <m.prawda99@gmail.com>
2020-06-28 16:15:01 +0200

    moved some div classes to id, added generic classes, works on tabs selection

--- static/ts/website-animations.ts
+++ static/ts/website-animations.ts
@@ -3,9 +3,12 @@
 
 var sideMenuButtons: Array<Element> = Array.from(document.getElementsByClassName("nav-wrapper"));
 var sheetSections: Array<Element> = Array.from(document.getElementsByClassName("sheet-section"));
+var displaySheetSections: Array<Element> = Array.from(document.getElementsByClassName("display-sheet-section"));
+var editSheetSections: Array<Element> = Array.from(document.getElementsByClassName("edit-sheet-section"));
+var clickedElement: number = 0;
 initializeSideMenu()
 
-// indexes 0-7 are for infomations about a character, 8,9 are for save and return
+// indexes 0-8 are for infomations about a character, 9-11 are for save, return and edit
 
 function initializeSideMenu(): void {
     sideMenuButtons.forEach(element => {
@@ -16,8 +19,17 @@ function initializeSideMenu(): void {
     });
     
     sheetSections[0].setAttribute("style", "display: block");
+    sideMenuButtons[0].setAttribute("style", "background: rgb(105, 105, 105)")
 }
 
 function changeSelectedInformations(): void {
-    console.log("test")
+    // this returns an anonymous type, Element needed
+    switch (sideMenuButtons.findIndex(this)) {
+        case 0:
+            console.log("0")
+        case 1:
+            console.log("1")
+        default:
+            console.log("def")
+    }
 }
