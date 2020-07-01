2507871cd9a9cc82512d501c38ced85737783cdf
pjlapinski <przemyslaw.j.lapinski@gmail.com>
2020-07-01 14:04:18 +0200

    fixed some comments

--- static/ts/char-sheet-updates.ts
+++ static/ts/char-sheet-updates.ts
@@ -35,11 +35,11 @@ for (let att of attributeScores) {
 for (let bonus of bonusesFields) {
   bonus.addEventListener('change', () => {
     let nameParts = bonus.id.split('-')
+    // removing the '-bonus-value' part
     nameParts.pop()
     nameParts.pop()
     let name: string = ''
     for (let part of nameParts) {
-      // first letter to upper case
       if (nameParts.indexOf(part) !== 0)
         part = part.charAt(0).toUpperCase() + part.slice(1)
       name += part
