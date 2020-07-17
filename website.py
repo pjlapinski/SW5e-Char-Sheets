d20425e467e8c817635b18596e80348e04356b88
pjlapinski <przemyslaw.j.lapinski@gmail.com>
2020-07-17 18:03:24 +0200

    added contact info to the footer

--- website.py
+++ website.py
@@ -86,9 +86,9 @@ def send_activation_email(user):
     # msg.html = 'message content', probably read from a file, or even better, a template like our pages
     # instead of msg.body
     msg.body = f"""Thank you for registering an account on our website. To activate it, visit this link: 
-    {url_for('activate', token=token, _external=True)}
-    This link will expire within 10 minutes of you recieving this message.
-    SW5e Character Sheets Team"""
+{url_for('activate', token=token, _external=True)}
+This link will expire within 10 minutes of you recieving this message.
+SW5e Character Sheets Team"""
     mail.send(msg)
 
 
@@ -101,7 +101,8 @@ def send_forgot_password_email(user):
     # msg.html = 'message content', probably read from a file, or even better, a template like our pages
     # instead of msg.body
     msg.body = f"""A password reset request has been sent. If you did not make this request, you can simply
-ignore this message. In other case, please enter this link to reset your password: {url_for('reset_password', token=token, _external=True)}
+ignore this message. In other case, please enter this link to reset your password:
+{url_for('reset_password', token=token, _external=True)}
 This link will expire within 10 minutes of you recieving this message.
 SW5e Character Sheets Team"""
     mail.send(msg)
