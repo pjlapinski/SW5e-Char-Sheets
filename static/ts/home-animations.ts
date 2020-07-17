e4ba5bd3b3abd697d53cb48898ee170487423d85
wohajo <m.prawda99@gmail.com>
2020-07-17 17:48:59 +0200

    styled select, added close button to modals, finished assigments

--- static/ts/home-animations.ts
+++ static/ts/home-animations.ts
@@ -7,6 +7,9 @@ function initializeModals(): void {
     let loginBtn = document.getElementById("login-button");
     let registerBtn = document.getElementById("register-button");
 
+    let loginCloseBtn = document.getElementById("login-close-btn");
+    let registerCloseBtn = document.getElementById("register-close-btn");
+
     let header = document.getElementsByClassName('header')[0]
     let footer = document.getElementsByClassName('footer')[0]
     let mainContent = document.getElementsByClassName('main-content-wrapper')[0]
@@ -22,11 +25,11 @@ function initializeModals(): void {
     })
 
     window.onclick = function(event) {
-        if (event.target == loginModal) {
+        if (event.target == loginModal || event.target == loginCloseBtn) {
             loginModal.style.display = "none";
             disableBlur();
         }
-        if (event.target == registerModal) {
+        if (event.target == registerModal || event.target == registerCloseBtn) {
             registerModal.style.display = "none";
             disableBlur();
         }
